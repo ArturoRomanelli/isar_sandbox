b0302573be0d6f4d90271e8b85af21f605cb3465
Arturo Romanelli <arturo.romanelli@wde.it>
2023-09-29 14:49:20 +0200

    Styled homepage

--- lib/src/cards/presentation/pages/cards_page.dart
+++ lib/src/cards/presentation/pages/cards_page.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:isar/isar.dart';
 
-import '../../domain/entities/game_card.dart';
+import '../../../../clients/local_client.dart';
+import '../../data/models/card_dto.dart';
+import '../../domain/adapters/card_adapter.dart';
+import '../widgets/game_card_widget.dart';
+import '../widgets/new_card_dialog.dart';
 
-class CardsPage extends StatelessWidget {
+class CardsPage extends HookConsumerWidget {
   const CardsPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isar = ref.watch(localDbProvider);
+
+    final cardList = isar.cardDtos.where().findAllSync();
+
+    final cardWidgets = [
+      ...cardList.map((dto) => dto.toEntity()).map(
+            (card) => GameCardWidget(
+              card: card,
+            ),
+          ),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Cards Against Humanity'),
@@ -17,71 +33,15 @@ class CardsPage extends StatelessWidget {
         crossAxisCount: 2,
         children: cardWidgets,
       ),
-    );
-  }
-}
-
-final cardList = [
-  const GameCard(
-    contents: "Generally, having no idea what's going on.",
-    eval: 5,
-    color: Colors.white,
-  ),
-  const GameCard(
-    contents: 'You are not alone. Millions of Americans struggle with _______ every day.',
-    eval: 3,
-    color: Colors.black,
-  ),
-];
-
-final cardWidgets = [
-  ...cardList.map(
-    (card) => GameCardWidget(
-      card: card,
-    ),
-  ),
-];
-
-class GameCardWidget extends HookWidget {
-  const GameCardWidget({
-    required this.card,
-    super.key,
-  });
-
-  final GameCard card;
-
-  @override
-  Widget build(BuildContext context) {
-    final rating = useState<double>(0);
-    return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-      color: card.color,
-      child: Padding(
-        padding: const EdgeInsets.all(8),
-        child: Column(
-          children: [
-            InkWell(
-              onTap: () {},
-              child: Text(
-                card.contents,
-                style: TextStyle(color: card.color == Colors.black ? Colors.white : Colors.black),
-              ),
-            ),
-            const Spacer(),
-            RatingBar.builder(
-              itemSize: 24,
-              itemBuilder: (context, _) => const Icon(
-                Icons.star,
-                color: Colors.amber,
-              ),
-              allowHalfRating: true,
-              initialRating: card.eval,
-              onRatingUpdate: (value) {
-                rating.value = value;
-              },
-            ),
-          ],
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.redAccent,
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
         ),
+        onPressed: () {
+          showDialog(context: context, builder: (_) => const NewCardDialog());
+        },
       ),
     );
   }
